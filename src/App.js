c6df60c2d28fdfcd3759cd7fbb36679faa3e687f
ivanovladyslav <ivanovladyslav@gmail.com>
2019-10-01 12:03:27 +0700

    Getting files and uploading files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import ScrollContainer from 'react-indiana-drag-scroll'
 import File from './components/file';
-import './app.css';
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -9,29 +9,57 @@ class App extends Component {
     this.state = {
       files: []
     }
+    this.getFiles = () => {
+      axios.get('http://127.0.0.1:4000/api')
+      .then((res, err) => {     
+        this.setState({
+          files: res.data
+        })   
+      })
+      .then(() => console.log(this.state))
+    }
+
+    this.onChangeHandler = (e) => {
+      const app = this;
+      const data = new FormData()
+      data.append('file', e.target.files[0])
+      console.log(data);
+      axios.post('http://127.0.0.1:4000/api/upload',
+        data
+      ).then(function(){
+        console.log('SUCCESS!!');
+        app.getFiles();
+      })
+      .catch(function(){
+        console.log('FAILURE!!');
+      });
+    }
   }
 
-  onChangeHandler = (e) => {
-    console.log(e.target.files[0])
-    const newFiles = [...this.state.files, e.target.files[0]];
-    this.setState({
-      files: newFiles
-    })
-    console.log(this.state);
+  componentDidMount() {
+    this.getFiles();
   }
+
 
   render() {
     const { files } = this.state;
     const filesToShow = files.map((item) => {
       return (
-        <File name={item.name} />
+        <File name={item.name} thumbnail={item.thumbnailLink}/>
       )
     })
 
     return (
       <div className="App">
-        <input className="btn-upload" type="file" onChange={this.onChangeHandler}/>
-        <ScrollContainer 
+        <form id="uploadForm" enctype="multipart/form-data">
+          <div className="btn-upload">
+            <img className="vector1" src="https://i.ibb.co/whWv1B8/Vector.png"></img>
+            <img className="vector2" src="https://i.ibb.co/2yg1z0M/Vector2.png"></img>
+            <p>Загрузить</p>
+            <input className="btn-input-hide" type="file" onChange={this.onChangeHandler}/>
+          </div>
+        </form>
+        {/* <ScrollContainer 
           className="scroll-container scrollable" 
           hideScrollbars={false}
           >
@@ -40,7 +68,14 @@ class App extends Component {
               {filesToShow}
             </div>
           </div>
-        </ScrollContainer>
+        </ScrollContainer> */}
+
+        <div className="workspace">
+              {filesToShow}
+        </div>
+        {/* <ScrollBox style={{height: '1000px; width: 1000px'}} axes={ScrollAxes.XY} fastTrack={FastTrack.PAGING}>
+          {filesToShow}
+        </ScrollBox> */}
       </div>
     );
   }
--- src/components/file.js
+++ src/components/file.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import Draggable from 'react-draggable';
 
 export default class File extends Component {
+
+    onClick = (e) => {
+        console.log("click");
+    }
 
     render() {
 
         return (
-            <div className="box box-1">{this.props.name}</div>
+            <Draggable>
+                <div onDoubleClick={this.onClick} className="box box-1">
+                    <img src={this.props.thumbnail} draggable="false"></img>
+                    {this.props.name}
+                </div>
+            </Draggable>
         )
     }
 }
